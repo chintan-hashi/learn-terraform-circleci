9cd68a142191d4d79b2730f102e1b38e73054e6b
chintan-hashi <57116381+chintan-hashi@users.noreply.github.com>
2020-05-28 20:00:12 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "remote" {
     hostname     = "app.terraform.io"
-    organization = "<YOUR-ORG-NAME>"
+    organization = "hc-se-demo-chintan"
 
     workspaces {
       name = "learn-terraform-circleci"
